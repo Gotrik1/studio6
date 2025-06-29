1020b0fe8cb6dc2892e4db85c88cef7f25e4eb78
Павел Кузнецов <gotrik.smith@gmail.com>
2025-06-29 19:40:11 +0000

    Что дальше?

--- src/devDependencies.ts
+++
@@ -1,1 +0,0 @@
-// This file is obsolete and can be deleted. It is not used in any build or development scripts.
--- src/features/donation-dialog.tsx
+++
@@ -1,1 +0,0 @@
-// This file is obsolete and can be deleted. The component is now located at /src/features/donation-dialog/index.tsx
