c490fae2c474bfc5f278e590c2845d5fde301eee
Павел Кузнецов <gotrik.smith@gmail.com>
2025-06-30 09:40:08 +0000

    сделай все границы белыми, сделай тень от карточек глубже, больше и темн

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -79,7 +79,7 @@ export default {
       boxShadow: {
         sm: "0 1px 2px 0 rgb(0 0 0 / 0.05)",
         DEFAULT: "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
-        md: "0 5px 10px -2px rgb(0 0 0 / 0.1), 0 3px 6px -3px rgb(0 0 0 / 0.1)",
+        md: "0 8px 16px -4px rgb(0 0 0 / 0.2), 0 4px 6px -2px rgb(0 0 0 / 0.1)",
         lg: "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)",
         xl: "0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)",
         '2xl': "0 25px 50px -12px rgb(0 0 0 / 0.25)",
