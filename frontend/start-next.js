71befafa4a72517c5cf51b093c0a10b9c8bf39c7
Павел Кузнецов <gotrik.smith@gmail.com>
2025-07-06 20:31:24 +0000

    докерфайл не надо обновить? пакейдж жсон?

--- frontend/start-next.js
+++ frontend/start-next.js
@@ -1,22 +1,2 @@
-const { spawn } = require('child_process');
-
-// Фильтруем только dev и turbopack
-const args = ['dev'];
-
-// Получаем порт и hostname из argv (с конца)
-const portArgIndex = process.argv.findIndex(arg => /^\d+$/.test(arg));
-const port = portArgIndex !== -1 ? process.argv[portArgIndex] : process.env.PORT || '9002';
-const hostname = process.argv[portArgIndex + 1] || process.env.HOSTNAME || '0.0.0.0';
-
-if (port) {
-  args.push('--port', port);
-}
-if (hostname) {
-  args.push('--hostname', hostname);
-}
-
-console.log('Launching Next.js with:', args);
-
-const next = spawn('npx', ['next', ...args], { stdio: 'inherit' });
-
-next.on('close', code => process.exit(code));
+// This file is now obsolete and can be removed.
+// The `next dev` command is now called directly from the package.json script.
