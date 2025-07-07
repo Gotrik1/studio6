547b966a79565c37c37c31d02be1ba569879ed45
Павел Кузнецов <gotrik.smith@gmail.com>
2025-07-07 19:16:58 +0000

    Ты используешь (и установил) старый пакет typescript-eslint (без @), кот

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,28 +1,35 @@
 
-const globals = require("globals");
-const tseslint = require("typescript-eslint");
-const prettierRecommended = require("eslint-plugin-prettier/recommended");
 const js = require("@eslint/js");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
+const prettier = require("eslint-config-prettier");
+const globals = require("globals");
 
-module.exports = tseslint.config(
-  {
-    ignores: ["dist/", ".eslintrc.js"],
-  },
+module.exports = [
   js.configs.recommended,
-  ...tseslint.configs.recommended,
-  prettierRecommended,
   {
+    files: ["**/*.ts"],
     languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+      },
       globals: {
         ...globals.node,
         ...globals.jest,
       },
     },
-    rules: {
-      '@typescript-eslint/interface-name-prefix': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
+    plugins: {
+      "@typescript-eslint": tsPlugin,
     },
-  }
-);
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      "@typescript-eslint/interface-name-prefix": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+  prettier,
+];
